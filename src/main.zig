feae571744190d56ee9fb901ba1db5ab0555bb8d
Janusz Piechówka <januszpiechowka@gmail.com>
2025-02-18 13:16:43 +0100

    Add initial code for logger lib

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 const lib_enigma = @import("enigma");
+const lib_logger = @import("logger");
 const lib_shadow = @import("shadow");
 const lib_syringe = @import("syringe");
 
 pub fn main() !void {
-    std.debug.print("Enigma: {}\n", .{lib_enigma.add(1, 2)});
-    std.debug.print("Shadow: {}\n", .{lib_shadow.add(3, 4)});
-    std.debug.print("Syringe: {}\n", .{lib_syringe.add(5, 6)});
+    lib_logger.info("Enigma: {}", .{lib_enigma.add(1, 2)});
+    lib_logger.warn("Shadow: {}", .{lib_shadow.add(3, 4)});
+    lib_logger.err("Syringe: {}", .{lib_syringe.add(5, 6)});
 
     lib_syringe.show_msg_box_winapi_test();
 }
